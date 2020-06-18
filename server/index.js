88d880fd86034c8e35489d3b3976cd53be42df07
ref34t <mohammedref34t@gmail.com>
2020-06-19 00:28:02 +0200

    Pass the Content-type in the request header

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const VideoRequestData = require('./data/video-requests.data');
 const UserData = require('./data/user.data');
 const cors = require('cors');
 const mongoose = require('./models/mongo.config');
-const multer = require('multer');
 
 if (!Object.keys(mongoose).length) return;
 
@@ -18,8 +17,7 @@ app.get('/', (req, res) =>
   res.send('Welcome to semicolon academy APIs, use /video-request to get data')
 );
 
-const upload = multer();
-app.post('/video-request', upload.none(), async (req, res, next) => {
+app.post('/video-request', async (req, res, next) => {
   const response = await VideoRequestData.createRequest(req.body);
   res.send(response);
   next();
